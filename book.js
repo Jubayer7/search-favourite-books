1428d6e124100f0aa20c249b89d1dcbad7fa3771
Jubayer Ahmed <sonniba7@gmail.com>
2021-09-02 22:28:11 +0600

    remain

--- book.js
+++ book.js
@@ -2,6 +2,19 @@
 const searchButton = () => {
     const searchArea = document.getElementById('input-area')
     const searchText = searchArea.value;
+    // NO result found
+    if (searchText == '') {
+        const noResult = document.getElementById('no-result')
+        noResult.innerText = 'No result found';
+        document.getElementById('result-found').style.display = 'none'
+    }
+    else {
+        const noResult = document.getElementById('no-result')
+        noResult.innerText = '';
+        document.getElementById('result-found').style.display = 'block'
+    }
+
+    // search remove area
     searchArea.value = '';
     const url = ` https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
@@ -10,25 +23,27 @@ const searchButton = () => {
 }
 
 const loasResult = books => {
-    console.log(books.length)
+    // Total result found
     const resultFound = document.getElementById('result-found')
+    // Display result Remove area
+    resultFound.textContent = '';
     const div = document.createElement('div')
     div.innerHTML = `
     <p>Total result found: ${books.length}</p>`
     resultFound.appendChild(div)
+    // Show display result
     const displayResult = document.getElementById('load-result')
-
     books.forEach(book => {
-        // console.log(book);
+        console.log(book);
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
         <div class="card">
-        <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="..."/>
+        <img  src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-100 img-fluid" alt="..."/>
         <div class="card-body">
-            <h5 class="card-title">${book.title}</h5>
-            <p class="card-text"> <span class="fw-bold">Author Name: ${book.author_name}</span> </p>
-            <p class="card-text"> <span class="fw-bold">Frist Publish Date: ${book.first_publish_year}</span> </p>
+            <h5 class="card-title text-primary">${book.title}</h5>
+            <p class="card-text text-success"> <span class="fw-bold ">Author Name: ${book.author_name}</span> </p>
+            <p class="card-text text-success"> <span class="fw-bold">Frist Publish Date: ${book.publish_date}</span> </p>
         </div>`
         displayResult.appendChild(div);
     })
